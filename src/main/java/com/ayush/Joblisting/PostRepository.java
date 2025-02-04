406883abb16a27f80deeb0ae1b4a67071f6246e0
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2025-02-04 22:18:10 +0530

    Created controller and model.Completed getAllposts function

---
+++ src/main/java/com/ayush/Joblisting/PostRepository.java
@@ -0,0 +1,7 @@
+package com.ayush.Joblisting;
+
+import com.ayush.Joblisting.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+}
